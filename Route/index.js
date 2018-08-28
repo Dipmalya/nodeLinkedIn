c9269b7f184f157173ffd764552d90f6496794ae
Dipmalya <dipmalyasen@gmail.com>
2018-08-28 16:34:40 +0530

    Update index.js

--- Route/index.js
+++ Route/index.js
@@ -22,6 +22,7 @@ app.get('/rest/api/users/get/:un', async (req, res) => {
 */
 app.put('/rest/api/users/addAward/:un', async (req, res) => {
     let result = await control.addAwards(req.params.un, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -31,6 +32,7 @@ app.put('/rest/api/users/addAward/:un', async (req, res) => {
 */
 app.put('/rest/api/users/changeAward/:un/:id', async (req, res) => {
     let result = await control.updateAwards(req.params.un, req.params.id, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -40,6 +42,7 @@ app.put('/rest/api/users/changeAward/:un/:id', async (req, res) => {
 */
 app.put('/rest/api/users/removeAward/:un/:id', async (req, res) => {
     let result = await control.removeAwards(req.params.un, req.params.id);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -49,6 +52,7 @@ app.put('/rest/api/users/removeAward/:un/:id', async (req, res) => {
 */
 app.put('/rest/api/users/addCertificate/:un', async (req, res) => {
     let result = await control.addCertifications(req.params.un, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -59,6 +63,7 @@ app.put('/rest/api/users/addCertificate/:un', async (req, res) => {
 
 app.put('/rest/api/users/changeCertificate/:un/:id', async (req, res) => {
     let result = await control.updateCertifications(req.params.un, req.params.id, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -69,6 +74,7 @@ app.put('/rest/api/users/changeCertificate/:un/:id', async (req, res) => {
 
 app.put('/rest/api/users/removeCertificate/:un/:id', async (req, res) => {
     let result = await control.removeCertifications(req.params.un, req.params.id);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -79,6 +85,7 @@ app.put('/rest/api/users/removeCertificate/:un/:id', async (req, res) => {
 
 app.put('/rest/api/users/addPublication/:un', async (req, res) => {
     let result = await control.addPublications(req.params.un, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -89,6 +96,7 @@ app.put('/rest/api/users/addPublication/:un', async (req, res) => {
 
 app.put('/rest/api/users/changePublication/:un/:id', async (req, res) => {
     let result = await control.updatePublications(req.params.un, req.params.id, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -99,6 +107,7 @@ app.put('/rest/api/users/changePublication/:un/:id', async (req, res) => {
 
 app.put('/rest/api/users/removePublication/:un/:id', async (req, res) => {
     let result = await control.removePublications(req.params.un, req.params.id, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -109,6 +118,7 @@ app.put('/rest/api/users/removePublication/:un/:id', async (req, res) => {
 
 app.put('/rest/api/users/addEndorsement/:un', async (req, res) => {
     let result = await control.addEndorsement(req.params.un, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -119,6 +129,7 @@ app.put('/rest/api/users/addEndorsement/:un', async (req, res) => {
 
 app.put('/rest/api/users/addSkill/:un/:skill', async (req, res) => {
     let result = await control.addSkill(req.params.un, req.params.skill);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -129,6 +140,7 @@ app.put('/rest/api/users/addSkill/:un/:skill', async (req, res) => {
 
 app.put('/rest/api/users/deleteSkill/:un/:skill', async (req, res) => {
     let result = await control.deleteSkill(req.params.un, req.params.skill);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -139,6 +151,7 @@ app.put('/rest/api/users/deleteSkill/:un/:skill', async (req, res) => {
 
 app.put('/rest/api/users/updateBio/:un', async (req, res) => {
     let result = await control.updateBio(req.params.un, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -149,6 +162,7 @@ app.put('/rest/api/users/updateBio/:un', async (req, res) => {
 
 app.put('/rest/api/users/addExperience/:un', async (req, res) => {
     let result = await control.addExperience(req.params.un, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -159,6 +173,7 @@ app.put('/rest/api/users/addExperience/:un', async (req, res) => {
 
 app.put('/rest/api/users/updateExperience/:un/:id', async (req, res) => {
     let result = await control.updateExperience(req.params.un, req.params.id, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -169,6 +184,7 @@ app.put('/rest/api/users/updateExperience/:un/:id', async (req, res) => {
 
 app.put('/rest/api/users/removeExperience/:un/:id', async (req, res) => {
     let result = await control.removeExperience(req.params.un, req.params.id);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -179,6 +195,7 @@ app.put('/rest/api/users/removeExperience/:un/:id', async (req, res) => {
 
 app.put('/rest/api/users/addEducation/:un', async (req, res) => {
     let result = await control.addEducation(req.params.un, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -189,6 +206,7 @@ app.put('/rest/api/users/addEducation/:un', async (req, res) => {
 
 app.put('/rest/api/users/updateEducation/:un/:id', async (req, res) => {
     let result = await control.updateEducation(req.params.un, req.params.id, req.body);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -199,6 +217,7 @@ app.put('/rest/api/users/updateEducation/:un/:id', async (req, res) => {
 
 app.put('/rest/api/users/removeEducation/:un/:id', async (req, res) => {
     let result = await control.removeEducation(req.params.un, req.params.id);
+    result = await control.getUserByUserName(req.params.un);
     res.send(result);
 });
 
@@ -210,6 +229,51 @@ app.put('/rest/api/users/removeEducation/:un/:id', async (req, res) => {
 app.get('/rest/api/users/countConnection/:un', async (req, res) => {
     let result = await control.countConnection(req.params.un);
     res.send(result);
+});
+
+/*
+    @desc : "This link will get the user's name and will call updateName()"
+    @author :  Somya Burman
+*/
+
+app.put('/rest/api/users/updateName/:un', async (req, res) => {
+    let result = await control.updateName(req.params.un, req.body);
+    result = await control.getUserByUserName(req.params.un);
+    res.send(result)
+});
+
+/*
+    @desc : "This link will get the user's name and will call updateDOB()"
+    @author :  Parag Badala
+*/
+
+
+app.put('/rest/api/users/updateDob/:un', async (req, res) => {
+    let result = await control.updateDOB(req.params.un, req.body);
+    result = await control.getUserByUserName(req.params.un);
+    res.send(result);
+});
+
+/*
+    @desc : "This link will get the user's name and will call upateEmail()"
+    @author :  Parag Badala
+*/
+
+app.put('/rest/api/users/updateEmail/:un', async (req,res) => {
+    let result = await control.updateEmail(req.params.un, req.body);
+    result = await control.getUserByUserName(req.params.un);
+    res.send(result)
+});
+
+/*
+    @desc : "This link will get the user's name and will call updateMobile()"
+    @author :  Parag Badala
+*/
+
+app.put('/rest/api/users/updateMobile/:un', async (req,res) => {
+    let result = await control.updateMobile(req.params.un, req.body);
+    result = await control.getUserByUserName(req.params.un);
+    res.send(result)
 });
 
 app.listen('4002', () => console.log('Listening on port 4002'))
